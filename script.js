acc54ee0e1d96a30b1c775395af9dc74187cb45d
eduardomoreira999 <114931062+eduardomoreira999@users.noreply.github.com>
2024-08-01 17:10:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+const Perguntas[
+    {
+        enunciado:"O que podemos fazer para ajudar a preservar o meio ambiente?"
+        caixaAlternativas:[
+            "não preservar as árvores"
+            "cuidar bem do seu lixo"
+        ]
+    }
+
+
+]
